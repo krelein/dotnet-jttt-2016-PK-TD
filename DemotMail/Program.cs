9c31a32145d5a37e79a2f2add6e93629ce416320
piry <piry3943@gmail.com>
2016-03-08 19:24:25 +0100

    Dodano klasę tworzącą log

--- DemotMail/Program.cs
+++ DemotMail/Program.cs
@@ -6,23 +6,32 @@ using System.Windows.Forms;
 using System.Net.Mail;
 using System.Text;
 using System.Net.Mime;
+using System.IO;
 namespace DemotMail
 {
+
+
     static class Program
     {
+ 
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+        
+            LogFile.StartLog();
+          
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             // Application.Run(new Form1());
-            Message M = new Message();
+         
+            Message M = new Message("demotmailtest@gmail.com", "dotNetC#");
             M.SetUrl("http://demotywatory.pl");
-            M.Send("piry3943@gmail.com", "a");
+            M.Send("piry3943@gmail.com", "marca");
 
+            LogFile.AddLog("Koniec działania programu");
         }
     }
 
